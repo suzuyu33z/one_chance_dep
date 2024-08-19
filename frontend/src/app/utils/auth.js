07f793d921e25062fbe7749ef9d99f8f0a3009ae
suzuki <suzuyu33z@gmail.com>
2024-08-19 21:50:38 +0900

    再度修正

--- frontend/src/app/utils/auth.js
+++ frontend/src/app/utils/auth.js
@@ -1,12 +1,15 @@
 // src/app/utils/auth.js
 export async function checkAuth() {
-  const response = await fetch(process.env.API_ENDPOINT + "/api/check-auth", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include", // セッションを含める
-  });
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_ENDPOINT + "/api/check-auth",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // セッションを含める
+    }
+  );
   const data = await response.json();
   return data;
 }
